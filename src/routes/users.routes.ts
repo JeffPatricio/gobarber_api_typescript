0e09d7c7e2693bdf2abc3364e1338faa87deae71
Jefferson Patrício <jeffersonpatricio1@gmail.com>
2020-07-18 18:42:15 -0300

    upload de avatar

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,14 +1,29 @@
 import { Router } from 'express';
+import multer from 'multer';
+import uploadConfig from '../config/upload';
 import CreateUserService from '../services/CreateUserService';
+import UpdateUserAvatarService from '../services/UpdateUserAvatarService';
+import ensureAuthenticated from '../middlewares/ensureAuthenticated';
 
 const usersRouter = Router();
+const upload = multer(uploadConfig);
 
 usersRouter.post('/', async (request, response) => {
   try {
-
     const { name, email, password } = request.body;
     const createUserService = new CreateUserService();
     const user = await createUserService.execute({ name, email, password });
+    delete user.password;
+    return response.json({ success: true, user });
+  } catch (err) {
+    response.status(400).json({ success: false, error: err.message });
+  }
+});
+
+usersRouter.patch('/avatar', ensureAuthenticated, upload.single('avatar'), async (request, response) => {
+  try {
+    const updateUserAvatarService = new UpdateUserAvatarService();
+    const user = await updateUserAvatarService.execute({ user_id: request.user.id, avatarFilename: request.file.filename });
     delete user.password;
     return response.json({ success: true, user });
 
