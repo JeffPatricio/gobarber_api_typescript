885850fd68173f1fdac3c8c2ccc259722e5a7bd6
Jefferson Patrício <jeffersonpatricio1@gmail.com>
2020-07-17 23:01:01 -0300

    inicio do projeto, implementacao de banco de dados postgresql

---
+++ src/routes/appointments.routes.ts
@@ -0,0 +1,27 @@
+import { Router } from 'express';
+import { getCustomRepository } from 'typeorm';
+import { parseISO } from 'date-fns';
+import AppointmentsRepository from '../repositories/AppointmentsRepository';
+import CreateAppointmentService from '../services/CreateAppointmentService';
+
+const appointmentsRouter = Router();
+
+appointmentsRouter.post('/', async (request, response) => {
+  try {
+    const { provider, date } = request.body;
+    const parsedDate = parseISO(date);
+    const createAppointment = new CreateAppointmentService();
+    const appointment = await createAppointment.execute({ date: parsedDate, provider });
+    return response.json({ appointment });
+  } catch (err) {
+    response.status(400).json({ error: err.message });
+  }
+});
+
+appointmentsRouter.get('/', async (request, response) => {
+  const appointmentsRepository = getCustomRepository(AppointmentsRepository);
+  const appointments = await appointmentsRepository.find();
+  return response.json({ appointments });
+});
+
+export default appointmentsRouter;
